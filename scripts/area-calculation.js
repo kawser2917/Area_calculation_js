5a2c8d64b1f9408624171517cadbce588bc67f56
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-10-02 10:51:37 +0600

    rectangle area calculated

--- scripts/area-calculation.js
+++ scripts/area-calculation.js
@@ -7,8 +7,28 @@ function calculateTraingleArea(){
     const heightString = heightElement.value
     const height = parseFloat(baseString)
 
-    const area = .5 * base * height
+    const traingleArea = .5 * base * height
 
-    const areaResult = document.getElementById('result')
-    areaResult.innerText = area
+    const areaResult = document.getElementById('traingle-result')
+    areaResult.innerText = traingleArea
+}
+
+function calculateRectangleArea(){
+    const widthElement = document.getElementById("rectangle-width")
+    const widthString = widthElement.value
+    const width = parseFloat(widthString)
+
+    const lengthElement = document.getElementById("rectangle-length")
+    const lengthString = lengthElement.value
+    const length = parseFloat(lengthString)
+
+    const rectangleArea = width * length
+
+    const rectangleResult = document.getElementById("rectangle-result")
+
+    rectangleResult.innerText = rectangleArea
+
+ 
+ 
+ 
 }
